ed4fb2c59fe6118822444ddaa4dfd7026448df0b
udan-insith <udaninsith23613@gmail.com>
2026-08-01 19:36:46 +0530

    Created a post request to the server and INSERT some VALUES

--- Products-API/server.js
+++ Products-API/server.js
@@ -10,3 +10,21 @@ let HTTP_PORT = 8080;
 app.listen(HTTP_PORT, () => {
   console.log("Server is running on %PORT%".replace("%PORT%", HTTP_PORT));
 });
+
+app.post("/api/products", (req, res, next) => {
+  const {
+    productName,
+    description,
+    category,
+    brand,
+    expireDate,
+    manufacturedDate,
+    batchNumber,
+    unitPrice,
+    quantity,
+    createdDate,
+  } = req.body;
+
+  var sql =
+    "INSERT INTO products (productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate) VALUES (?,?,?,?,?,?,?,?,?,?)";
+});
